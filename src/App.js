b0d33277f81a1acab3649e8be0fa7de86119536f
srjn0201 <srjn0201@gmail.com>
2024-11-16 00:58:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://sarjil.site"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mohd Sharjeel
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
